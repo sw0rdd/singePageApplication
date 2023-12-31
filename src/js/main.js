972a955975549605344224d906614221580a0bd2
Seif-Alamir Yousef <sy222ea@student.lnu.se>
2023-12-31 19:45:50 +0100

    made a function to take grid input for game

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 'use strict'
 
-
-import * as window from './modules/window.js'; // You'll create this file next
+import * as game from './modules/memoryGame.js';
 import * as chat from './modules/chat.js';
 import * as weatherApp from './modules/weather.js';
 
-document.querySelector('.start_memory').addEventListener('click', () => createWindow('Memory Game'));
+document.querySelector('.start_memory').addEventListener('click', () => game.createGridInputWindow('Memory Game'));
 document.querySelector('.start_chat').addEventListener('click', () => chat.initChat());
 document.querySelector('.start_weather').addEventListener('click', () => weatherApp.createWeatherWindow('Weather App'));
 
