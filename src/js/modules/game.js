7d89276ab183fb86d574f62bd66b45eb72deede2
Seif-Alamir Yousef <sy222ea@student.lnu.se>
2024-01-01 00:31:55 +0100

    memory game working, must style

--- src/js/modules/game.js
+++ src/js/modules/game.js
@@ -1,10 +1,7 @@
 import * as window from './window.js';
+import * as memoryGame from './memoryGame.js';
 
-
-
-let playerLives = 6 
-let size = '';
-
+let size
 
 export function createGridInputWindow(title) {
 
@@ -63,9 +60,8 @@ export function createGridInputWindow(title) {
             return;
         } else {
             document.querySelector('#input_box').remove();
-            // card generator
-            CreateGame(size);
-
+            let game = new memoryGame.MemoryGame(size);
+            game.CreateGame();
         }
     })
 
@@ -73,194 +69,11 @@ export function createGridInputWindow(title) {
 }
 
 
-export function getData(size) {
-    console.log(size)
-    const baseData = [
-        { imgSrc: './img/memory_game/0.png', name: '0' },
-        { imgSrc: './img/memory_game/1.png', name: '1' },
-        { imgSrc: './img/memory_game/2.png', name: '2' },
-        { imgSrc: './img/memory_game/3.png', name: '3' },
-        { imgSrc: './img/memory_game/4.png', name: '4' },
-        { imgSrc: './img/memory_game/5.png', name: '5' },
-        { imgSrc: './img/memory_game/6.png', name: '6' },
-        { imgSrc: './img/memory_game/7.png', name: '7' },
-        { imgSrc: './img/memory_game/0.png', name: '0' },
-        { imgSrc: './img/memory_game/1.png', name: '1' },
-        { imgSrc: './img/memory_game/2.png', name: '2' },
-        { imgSrc: './img/memory_game/3.png', name: '3' },
-        { imgSrc: './img/memory_game/4.png', name: '4' },
-        { imgSrc: './img/memory_game/5.png', name: '5' },
-        { imgSrc: './img/memory_game/6.png', name: '6' },
-        { imgSrc: './img/memory_game/7.png', name: '7' },
-    ]
-
-    let numOfPairs
-    switch (size) {
-        case '4x4':
-            numOfPairs = 8
-            break;
-        case '2x2':
-            numOfPairs = 2
-            break;
-        case '2x4':
-            numOfPairs = 4
-            break;
-    
-        default:
-            throw new Error('invalid size')
-    }
-
-    const selectedImages = baseData.slice(0, numOfPairs)
-    const cardData = selectedImages.concat(selectedImages)
-
-    return cardData;
-
-}
-
-
-function randomize(size) {
-    const cardData = getData(size);
-    cardData.sort(() => Math.random() - 0.5);
-    console.log(cardData)
-    return cardData;
-}
 
 
 
 
 
 
-export function CreateGame(size) {
-
-    const windowElement = document.createElement('div');
-    windowElement.classList.add('window');
-
-    const titleBar = window.makeTitleBar('Memory Game', windowElement);
-
-    const closeButton = document.createElement('button');
-    closeButton.classList.add('close_button');
-    closeButton.innerHTML = '<img src="img/close2.png" alt="close icon">';
-
-    closeButton.addEventListener('click', () => {
-        windowElement.remove();
-    });
-
-    titleBar.appendChild(closeButton);
-    windowElement.appendChild(titleBar);
-
-
-    const memory_game = document.createElement('div');
-    memory_game.classList.add('memory_game');
-
-    // this hedaer has lives and reset button
-    const game_header = document.createElement('div');
-    game_header.classList.add('game_header');
-
-
-    const playerLivesCount = document.createElement('h1');
-    playerLivesCount.classList.add('player_lives_count');
-    game_header.appendChild(playerLivesCount);
-
-    const section = document.createElement('section');
-
-
-    const cardData = randomize(size);
-
-    cardData.forEach((item) => {
-        const memory_card = document.createElement('div');
-        const face = document.createElement('img');
-        const back = document.createElement('img');
-        memory_card.classList = 'memory_card';
-        face.classList = 'face';
-        back.classList = 'back';
-
-        // attach the info to the cards
-        face.src = item.imgSrc;
-        memory_card.setAttribute('name', item.name);
-
-        back.src = './img/memory_game/question_mark.png';
-        back.setAttribute('name', 'question_mark');
-
-        // attach the card to the section
-        section.appendChild(memory_card)
-        memory_card.appendChild(face);
-        memory_card.appendChild(back);
-
-        memory_card.addEventListener('click', (e) => {
-            memory_card.classList.toggle('togglCard');
-            console.log('card clicked')
-            checkCards(e);
-        })
-
-    })
-
-    memory_game.appendChild(game_header);
-    memory_game.appendChild(section);
-    windowElement.appendChild(memory_game);
-    document.querySelector('main').appendChild(windowElement);
-    window.makeDraggable(windowElement);
-
-    playerLivesCount.textContent = playerLives;
-
-}
-
-// check cards function
-function checkCards(e) {
-    const clickedCard = e.target;
-    clickedCard.classList.add('flipped');
-
-    const flippedCards = document.querySelectorAll('.flipped');
-    const togglCards = document.querySelectorAll('.togglCard');
-
-    // logic 
-    if (flippedCards.length === 2) {
-        if (flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')) {
-            flippedCards.forEach((card) => {
-                card.classList.remove('flipped')
-                card.style.pointerEvents = 'none'
-            })
-        } else {
-            console.log("wrong")
-            flippedCards.forEach((card) => {
-                card.classList.remove('flipped');
-                setTimeout(() => card.classList.remove('togglCard'), 1000)
-            })
-            playerLives--;
-            document.querySelector('.player_lives_count').textContent = playerLives;
-            if (playerLives === 0) {
-                reset('Try again!');
-            }
-        }
-    }
-
-    // check if won
-    if (togglCards.length === 16) {
-        reset('You won!')
-    }
-    
-    
-}
-
-function reset(message) {
-    const cardData = randomize();
-    let faces = document.querySelectorAll('.face');
-    let cards = document.querySelectorAll('.memory_card');
-
-    section.style.pointerEvents = 'none';
-
-    cardData.forEach((item, index) => {
-        cards[index].classList.remove('togglCard');
-        setTimeout(() => {
-            cards[index].style.pointerEvents = 'all';
-            faces[index].src = item.imgSrc;
-            cards[index].setAttribute('name', item.name);
-            section.style.pointerEvents = 'all'
-        }, 1000)
-    })
-
-    playerLives = 6;
-    document.querySelector('.player_lives_count').textContent = playerLives;
-    setTimeout(() => window.alert(message), 100)
-}
 
 
--- src/js/modules/memoryGame.js
+++ src/js/modules/memoryGame.js
@@ -1,114 +1,212 @@
-'use strict'
+import * as windowManager from './window.js';
 
-import * as window from './window.js';
-
-let submissionWindowShown = false;
-let size = '';
-
-
-function createGridInputWindow(title) {
-
-    if (submissionWindowShown) {
-        alert('You already have a submission window open!');
-        return
+export class MemoryGame {
+    constructor(size) {
+        this.size = size;
+        this.cardData = this.randomize();
+        this.playerLives = 6
+        this.gameElement = null
     }
 
-    const windowElement = document.createElement('div');
-    windowElement.classList.add('window');
-    windowElement.id = 'input_box';
+    randomize() {
+        const cardData = this.getData(this.size);
+        cardData.sort(() => Math.random() - 0.5);
+        return cardData;
+    }
 
-    const titleBar = window.makeTitleBar(title, windowElement);
+    getData() {
+        const baseData = [
+            { imgSrc: './img/memory_game/0.png', name: '0' },
+            { imgSrc: './img/memory_game/1.png', name: '1' },
+            { imgSrc: './img/memory_game/2.png', name: '2' },
+            { imgSrc: './img/memory_game/3.png', name: '3' },
+            { imgSrc: './img/memory_game/4.png', name: '4' },
+            { imgSrc: './img/memory_game/5.png', name: '5' },
+            { imgSrc: './img/memory_game/6.png', name: '6' },
+            { imgSrc: './img/memory_game/7.png', name: '7' },
+            { imgSrc: './img/memory_game/0.png', name: '0' },
+            { imgSrc: './img/memory_game/1.png', name: '1' },
+            { imgSrc: './img/memory_game/2.png', name: '2' },
+            { imgSrc: './img/memory_game/3.png', name: '3' },
+            { imgSrc: './img/memory_game/4.png', name: '4' },
+            { imgSrc: './img/memory_game/5.png', name: '5' },
+            { imgSrc: './img/memory_game/6.png', name: '6' },
+            { imgSrc: './img/memory_game/7.png', name: '7' },
+        ]
 
-    const closeButton = document.createElement('button');
-    closeButton.classList.add('close_button');
-    closeButton.innerHTML = '<img src="img/close2.png" alt="close icon">';
+        let numOfPairs
+        switch (this.size) {
+            case '4x4':
+                numOfPairs = 8
+                break;
+            case '2x2':
+                numOfPairs = 2
+                break;
+            case '2x4':
+                numOfPairs = 4
+                break;
 
-    closeButton.addEventListener('click', () => {
-        windowElement.remove();
-        submissionWindowShown = false;
-    });
-
-    titleBar.appendChild(closeButton);
-    windowElement.appendChild(titleBar);
-
-    const gameInputBox = document.createElement('div');
-    gameInputBox.id = 'input_box';
-    gameInputBox.classList.add('user_box');
-
-    const userForm = document.createElement('form');
-    const userLabel = document.createElement('label');
-    userLabel.htmlFor = 'game_grid';
-    userLabel.textContent = 'Enter the game grid: ';
-    const userInput = document.createElement('input');
-    userInput.type = 'text';
-    userInput.id = 'game_grid';
-    userInput.name = 'game_grid';
-    const userButton = document.createElement('button');
-    userButton.type = 'submit';
-    userButton.id = 'submit_grid';
-    userButton.classList.add('button');
-    userButton.textContent = 'Submit!';
-    userForm.appendChild(userLabel);
-    userForm.appendChild(userInput);
-    userForm.appendChild(userButton);
-    gameInputBox.appendChild(userForm);
-
-    windowElement.appendChild(gameInputBox);
-
-    document.querySelector('main').appendChild(windowElement);
-    window.makeDraggable(windowElement);
-
-    document.querySelector('#submit_grid').addEventListener('click', (event) => {
-        event.preventDefault();
-        size = parseInt(document.querySelector('#game_grid').value);
-
-        if (isNaN(size) || size % 2 !== 0 || size < 2 || size > 16) {
-            alert('Please enter a valid game grid!');
-            return;
-        } else {
-            document.querySelector('#input_box').remove();
-            // create game window
+            default:
+                throw new Error('invalid size')
         }
-    })
 
-    document.querySelector('#game_grid').focus();
-    submissionWindowShown = true;
-}
+        const selectedImages = baseData.slice(0, numOfPairs)
+        const cardData = selectedImages.concat(selectedImages)
 
+        return cardData;
 
-function createMemoryGameBoard(size) {
-    const totalCards = size * size;
-    const pairs = totalCards / 2;
-    let boardArray = [];
-
-    // Populate the board array with pairs
-    for (let i = 0; i < pairs; i++) {
-        boardArray.push(i, i);
     }
 
-    // Shuffle the board array
-    shuffleArray(boardArray);
 
-    // Create HTML elements for the board
-    const boardElement = document.createElement('div');
-    boardElement.classList.add('memory-board');
+    CreateGame() {
 
-    boardArray.forEach(cardNumber => {
-        const cardElement = document.createElement('div');
-        cardElement.classList.add('memory-card');
-        cardElement.dataset.number = cardNumber;
-        // Add event listeners for click and keyboard
-        // ...
+        const windowElement = document.createElement('div');
+        windowElement.classList.add('window');
 
-        boardElement.appendChild(cardElement);
-    });
+        const titleBar = windowManager.makeTitleBar('Memory Game', windowElement);
 
-    return boardElement;
-}
+        const closeButton = document.createElement('button');
+        closeButton.classList.add('close_button');
+        closeButton.innerHTML = '<img src="img/close2.png" alt="close icon">';
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+        closeButton.addEventListener('click', () => {
+            windowElement.remove();
+        });
+
+        titleBar.appendChild(closeButton);
+        windowElement.appendChild(titleBar);
+
+        this.gameElement = windowElement;
+
+        const memory_game = document.createElement('div');
+        memory_game.classList.add('memory_game');
+
+        // this hedaer has lives and reset button
+        const game_header = document.createElement('div');
+        game_header.classList.add('game_header');
+
+
+        const playerLivesCount = document.createElement('h1');
+        playerLivesCount.classList.add('player_lives_count');
+        game_header.appendChild(playerLivesCount);
+
+        const section = document.createElement('section');
+        this.section = section
+
+
+        const cardData = this.randomize();
+
+        cardData.forEach((item) => {
+            const memory_card = document.createElement('div');
+            const face = document.createElement('img');
+            const back = document.createElement('img');
+            memory_card.classList = 'memory_card';
+            face.classList = 'face';
+            back.classList = 'back';
+
+            // attach the info to the cards
+            face.src = item.imgSrc;
+            memory_card.setAttribute('name', item.name);
+
+            back.src = './img/memory_game/question_mark.png';
+            back.setAttribute('name', 'question_mark');
+
+            // attach the card to the section
+            section.appendChild(memory_card)
+            memory_card.appendChild(face);
+            memory_card.appendChild(back);
+
+            memory_card.addEventListener('click', (e) => {
+                memory_card.classList.toggle('togglCard');
+                this.checkCards(e, memory_card)
+            })
+
+        })
+
+        memory_game.appendChild(game_header);
+        memory_game.appendChild(section);
+        windowElement.appendChild(memory_game);
+        document.querySelector('main').appendChild(windowElement);
+        windowManager.makeDraggable(windowElement);
+
+        playerLivesCount.textContent = `Player Lives: ${this.playerLives}`
+
     }
+
+    // check cards function
+    checkCards(e, clickedCard) {
+        clickedCard.classList.add('flipped');
+
+        const flippedCards = this.gameElement.querySelectorAll('.flipped');
+        const togglCards = this.gameElement.querySelectorAll('.togglCard');
+
+        // logic 
+        if (flippedCards.length === 2) {
+            if (flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')) {
+                flippedCards.forEach((card) => {
+                    card.classList.remove('flipped')
+                    card.style.pointerEvents = 'none'
+                })
+            } else {
+                flippedCards.forEach((card) => {
+                    card.classList.remove('flipped');
+                    setTimeout(() => card.classList.remove('togglCard'), 1000)
+                })
+                this.playerLives--;
+                this.gameElement.querySelector('.player_lives_count').textContent = `Player Lives: ${this.playerLives}`;
+                if (this.playerLives === 0) {
+                    setTimeout(() => this.reset('You lost!'), 1000)
+                    return
+                }
+            }
+        }
+
+        // check if won
+        setTimeout(() => {
+            const totalPairs = this.cardData.length / 2;
+            if (togglCards.length === totalPairs * 2) {
+                this.reset('You won!')
+            }
+        }, 1000)
+    }
+
+    reset(message) {
+        const cardData = this.randomize();
+        let faces = this.gameElement.querySelectorAll('.face');
+        let cards = this.gameElement.querySelectorAll('.memory_card');
+    
+        this.section.style.pointerEvents = 'none';
+    
+        cardData.forEach((item, index) => {
+            cards[index].classList.remove('togglCard');
+            setTimeout(() => {
+                cards[index].style.pointerEvents = 'all';
+                faces[index].src = item.imgSrc;
+                cards[index].setAttribute('name', item.name);
+                this.section.style.pointerEvents = 'all'
+            }, 1000)
+        })
+    
+        this.playerLives = 6;
+        this.gameElement.querySelector('.player_lives_count').textContent = `Player Lives: ${this.playerLives}`;
+        setTimeout(() => window.alert(message), 100)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
